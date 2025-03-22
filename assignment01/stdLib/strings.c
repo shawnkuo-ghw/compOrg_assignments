f15340682c9859487ef9876a75f31fb72bea027c
Onism <mingshan466@gmail.com>
2025-03-22 22:11:10 +0800

    三strings.c done by lms

--- assignment01/stdLib/strings.c
+++ assignment01/stdLib/strings.c
@@ -1,32 +1,28 @@
 #include "standard.h"
 #include <stdlib.h>
   
-    //Return the length of the string
-unsigned int length(const char * string) {
 
+unsigned int length(const char * string) {
+    // DONE by lms
     unsigned int len = 0;
-    while (string[len] != '\0') {  // until we meet '\0'
+    while (string[len] != '\0') {
         len++;
     }
     return len;
 }
 
 
-    //Get string1 and string2,return if they are equeal
 boolean equals(const char * string1, const char * string2) {
-    
+    // DONE by lms    
     unsigned int i = 0;
-    while (string1[i] != '\0' && string2[i] != '\0') {  // keep compair，until '\0'
-        if (string1[i] != string2[i]) 
-            return FALSE;  // if some words not equal，return FALSE
+    while (string1[i] != '\0' && string2[i] != '\0') {  
+            return FALSE;  
         i++;
     }
-    return string1[i] == string2[i];  // check if they are end at the same time
+    return string1[i] == string2[i];  
 }
 
 
-
-    //Find the index of position of e(first appear).
 int index_of(const char * string, const char e) {
     int idx = 0;
     boolean found = FALSE;
@@ -39,16 +35,16 @@ int index_of(const char * string, const char e) {
     return found?idx:-1;
 }
 
-    //Find the index of position of e last appear
+
 int last_index_of(const char * string, const char e) {
-   
-    int idx = -1;  // initial -1(not find)
-    for (int i = 0; string[i] != '\0'; i++) {  // cover all the string
+    // DONE by lms
+    int idx = -1;  
+    for (int i = 0; string[i] != '\0'; i++) {  
         if (string[i] == e) {
-            idx = i;  // update index be the position of i word
+            idx = i;  
         }
     }
-    return idx;  // retrun the last appear  e's index，if not find then return -1
+    return idx;  
 }
 
 
@@ -61,52 +57,53 @@ int last_index_of(const char * string, const char e) {
 
 #define CASE_DIFF 32
 
-// 'A' to 'a'
+
 char * to_lower_case(const char * string) {
+    // DONE by lms
     unsigned int len = length(string);
-    char * result = malloc(len + 1);  // allocate memory for the result string
+    char * result = malloc(len + 1);  
     for (unsigned int i = 0; i < len; i++) {
-        result[i] = shift_in_range(string[i], UPPER_CASE_MIN, UPPER_CASE_MAX, CASE_DIFF);  // 'A' to 'a'
+        result[i] = shift_in_range(string[i], UPPER_CASE_MIN, UPPER_CASE_MAX, CASE_DIFF);  
     }
-    result[len] = '\0';  // add the end '\0'
+    result[len] = '\0';  
     return result;
 }
 
-// 'a' to 'A'
+
 char * to_upper_case(const char * string) {
- 
+    // DONE by lms
     unsigned int len = length(string);
-    char * result = malloc(len + 1);  // allocate memory for the result string
+    char * result = malloc(len + 1);  
     for (unsigned int i = 0; i < len; i++) {
-        result[i] = shift_in_range(string[i], UPPER_CASE_MIN, UPPER_CASE_MAX, -CASE_DIFF);  // 'a' to 'A'
+        result[i] = shift_in_range(string[i], UPPER_CASE_MIN, UPPER_CASE_MAX, -CASE_DIFF);  
     }
-    result[len] = '\0';  // 添加字符串结尾的 '\0'
+    result[len] = '\0';  
     return result;
 }
   
 
-    //Switch the word
+    
 static char shift_in_range(const char c, unsigned int range_start, unsigned int range_end, const int shift) {
-   
-    if (c >= range_start && c <= range_end) {  // if the character is legal
-        return c + shift;  // shift the word c,using the CASE_DIFF
+    // DONE by lms
+    if (c >= range_start && c <= range_end) {  
+        return c + shift;  
     }
-    return c;  // if the character is not within the range,return the original character directly
+    return c;  
 }
 
     
-    //Return a substring 
-char * substring(const char * string, unsigned int from, unsigned int to) {
     
+char * substring(const char * string, unsigned int from, unsigned int to) {
+    // DONE by lms
     if (from >= to || from >= length(string)) {
-        return NULL;  // if the index is wrong，return NULL
+        return NULL;  
     }
     unsigned int len = to - from;
-    char * result = malloc(len + 1);  // allocate memory for the substring
+    char * result = malloc(len + 1);  
     for (unsigned int i = 0; i < len; i++) {
-        result[i] = string[from + i];  // copy the substring
+        result[i] = string[from + i];  
     }
-    result[len] = '\0';  // add the '\0' to the end
+    result[len] = '\0';  
     return result;
 }
 
